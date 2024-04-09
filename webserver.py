609d3e834e12f186a3ebe8de4d7e633bc5d5b9ff
blaze361 <166518225+blaze361@users.noreply.github.com>
2024-04-09 11:04:26 -0400

    Update webserver.py

--- webserver.py
+++ webserver.py
@@ -7,11 +7,11 @@ hostName = "10.1.165.2"
 serverPort = 9851
 class myServer(BaseHTTPRequestHandler):  
   def do_GET(self): #the do_GET method is inherited from BaseHTTPRequestHandler 
-    self.send_response(200)    
+    self.send_response(200, "OK")    
     self.send_header("Content-type", "application/json")
     self.end_headers()
   def do_POST(self):
-    self.send_response(200)
+    self.send_response(200, "OK")
     self.send_header("Content-type", "application/json")
     self.end_headers()
     message = int(self.headers.get("Content-Length"))
