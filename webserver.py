8e677ac45703edc2103aaa017dd0ddd23b65985c
blaze361 <166518225+blaze361@users.noreply.github.com>
2024-04-09 10:10:15 -0400

    Update webserver.py

--- webserver.py
+++ webserver.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
   webServer = HTTPServer((hostName, serverPort), myServer)
   print("Server started http://%s:%s" % (hostName, serverPort))  #Server starts
 try:
-          webServer.serve_forever()
+  webServer.serve_forever()
 except KeyboardInterrupt:
   pass
 webServer.server_close()  #Executes when you hit a keyboard interrupt, closing the server
