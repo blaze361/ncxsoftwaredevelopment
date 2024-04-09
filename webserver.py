23b9d5b47b871f6d1d9e95785760d4e144842784
blaze361 <166518225+blaze361@users.noreply.github.com>
2024-04-09 09:50:37 -0400

    Update webserver.py

--- webserver.py
+++ webserver.py
@@ -1,28 +1,28 @@
 import time
-import requests
-import BaseHTTPRequestHandler
-import SimpleHTTPServer
+import http
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
 
 hostName = "localhost"
 serverPort = 9851
-Class MyServer(BaseHTTPRequestHandler):  
-  def do_GET(self): //the do_GET method is inherited from BaseHTTPRequestHandler 
+class myServer(BaseHTTPRequestHandler):  
+  def do_GET(self): #the do_GET method is inherited from BaseHTTPRequestHandler 
     self.send_response(200)    
-              self.send_header("Content-type", "text/html")
-              self.end_headers()
-              self.wfile.write(bytes("<html><head><title>https://testserver.com</title></head>", "utf-8"))
-              self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
-              self.wfile.write(bytes("<body>", "utf-8"))
-              self.wfile.write(bytes("<p>This is an example web server.</p>", "utf-8"))
-              self.wfile.write(bytes("</body></html>", "utf-8"))
+    self.send_header("Content-type", "text/html")
+    self.end_headers()
+    self.wfile.write(bytes("<html><head><title>https://testserver.com</title></head>", "utf-8"))
+    self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
+    self.wfile.write(bytes("<body>", "utf-8"))
+    self.wfile.write(bytes("<p>This is an example web server.</p>", "utf-8"))
+    self.wfile.write(bytes("</body></html>", "utf-8"))
     self.send_respose(400)
 
 if __name__ == "__main__":        
-  webServer = HTTPServer((hostName, serverPort), MyServer)
-             print("Server started http://%s:%s" % (hostName, serverPort))  #Server starts
+  webServer = HTTPServer((hostName, serverPort), myServer)
+  print("Server started http://%s:%s" % (hostName, serverPort))  #Server starts
 try:
           webServer.serve_forever()
 except KeyboardInterrupt:
-pass
+  pass
 webServer.server_close()  #Executes when you hit a keyboard interrupt, closing the server
-    print("Server stopped.")
+print("Server stopped.")
